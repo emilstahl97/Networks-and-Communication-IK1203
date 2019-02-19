b4faa6b92ceb0b45e3256ec9c5981d1004b51454
Emil Ståhl <emil.stahl@icloud.com>
2019-02-19 20:39:32 +0100

    commented code

--- task2/HTTPEcho.java
+++ task2/HTTPEcho.java
@@ -1,3 +1,9 @@
+/* This program echos the HTTP request sent by the web browser and displays it as text output
+ * it takes in a port as an argument and opens up a connection to that specific port.
+
+@author Emil Stahl
+*/ 
+
 import java.net.*;
 import java.io.*;
 
@@ -5,32 +11,35 @@ public class HTTPEcho {
 
 	public static void main( String[] args) throws Exception {
 
+		//store the clients data in the string toServer
 		String toServer; 
-		String Echo;
-		int port = Integer.parseInt(args[0]);
-		ServerSocket HTTPSocket = new ServerSocket(port); 
+		//open a serversocket with the port given by the user
+		ServerSocket HTTPSocket = new ServerSocket(Integer.parseInt(args[0])); 
+		//Header to be appended first
+		String Header = "HTTP/1.1 200 OK\r\n\r\n";
 		
 		while(true) {
 			
+			//Create socket and accept client
 			Socket connection = HTTPSocket.accept();
+			//open bufferreader and outputstream 
 			BufferedReader clientInput = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			DataOutputStream clientOutput = new DataOutputStream(connection.getOutputStream());
-			StringBuilder sb = new StringBuilder();
-			boolean statusLine = true; 
+			//The echo from server that is being returned to the client
+			StringBuilder Echo = new StringBuilder();
+			//The echo needs to begin with the http header
+			Echo.append(Header);
 			
+			//while there's input, continue reading 
 			while(((toServer = clientInput.readLine()) != null) && (toServer.length() != 0))
 			{
-				if(statusLine) 
-				{
-					sb.append("HTTP/1.1 200 OK\r\n\r\n");
-					statusLine = false;
-				}
-				sb.append(toServer + "\r\n");
+				Echo.append(toServer + "\r\n"); //append it to Echo
 			}
 			
-			Echo = sb.toString();
-			clientOutput.writeBytes(Echo);
+			//return the input as an echo
+			clientOutput.writeBytes(Echo.toString());
 			
+			//close the sockets and streams
 			clientInput.close();
 			clientOutput.close();
 			connection.close();
